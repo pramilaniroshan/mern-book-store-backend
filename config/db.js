8624c0e9956731a3346d44123c53893a11e1df2b
pramilaniroshan <pramilaniroshan@gmail.com>
2021-05-17 16:39:50 +0530

    server completed

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@
   const config = require('config');
   const db = config.get('mongoURI');
 
-  //const db = "mongodb+srv://admin:<Ad#PF57L8W5wAgN>@book-store.0fa86.mongodb.net/book-store?retryWrites=true&w=majority"
+  
   mongoose.set('useUnifiedTopology', true);
   
   const connectDB = async () => {
